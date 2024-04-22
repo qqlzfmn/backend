0de6f0fcde7a972cb3a5281c05a6450cbd76ff27
LiZongfeng <lzf940629@vip.qq.com>
2024-04-22 06:51:48 +0000

    add some test case

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 import flask
+import json
 
 app = flask.Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/hello', methods=['POST'])
+def hello():
+    data = json.loads(flask.request.data)
+    name = data.get('name', 'World')
+    return f'Hello, {name}!'
+
+@app.route('/<string:name>', methods=['GET'])
+def hello_name(name):
+    return f'Hello, {name}!'
 
 if __name__ == '__main__':
     app.run(debug=True)
